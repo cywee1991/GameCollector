52f04c9508713b7ec4478eceeddd922f14ff96d1
Chan Yan wee <cywee1991@gmail.com>
2018-11-22 22:10:22 +0800

    Initial Commit

---
+++ GameCollector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameCollector
+//
+//  Created by Chan Yan wee on 22/11/18.
+//  Copyright © 2018 Chan Yan wee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
